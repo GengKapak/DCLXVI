fa3eaa7e7e621b5a165030f8d704e061ae430050
Angga <angga@linuxmail.org>
2020-05-18 17:35:24 +0700

    DCLXVI: add tagall module
    Signed-off-by: Angga <angga@linuxmail.org>

---
+++ userbot/modules/tagall.py
@@ -0,0 +1,28 @@
+from telethon import events
+from asyncio import sleep
+from random import choice, getrandbits, randint
+from re import sub
+from collections import deque
+import requests
+
+from userbot import CMD_HELP, bot
+from userbot.events import register
+from userbot.modules.admin import get_user_from_event
+
+@register(outgoing=True, pattern="^.all$")
+async def all(event):
+    if event.fwd_from:
+        return
+    await event.delete()
+    mentions = "⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣؜"
+    chat = await event.get_input_chat()
+    async for x in bot.iter_participants(chat, 100):
+        mentions += f"[\u2063](tg://user?id={x.id})"
+    await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
+
+
+CMD_HELP.update({
+    "all":
+    ">.all"
+    "\nUsage: A Plugin to tagall in the chat."
+})
