af3dc6cb3ecd4e2e52ca412313b8bc3446cb397e
AnggaR96s <angga@linuxmail.org>
2020-05-18 14:58:32 +0700

    DCLXVI: Let's started
    Signed-off-by: AnggaR96s <angga@linuxmail.org>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -6,22 +6,54 @@
 """ Userbot initialization. """
 
 import os
+import re
 
 from sys import version_info
 from logging import basicConfig, getLogger, INFO, DEBUG
 from distutils.util import strtobool as sb
+from math import ceil
 
 from pylast import LastFMNetwork, md5
 from pySmartDL import SmartDL
 from dotenv import load_dotenv
 from requests import get
-from telethon import TelegramClient
+from telethon.sync import TelegramClient, custom, events
+from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.sessions import StringSession
-
+from telethon.utils import get_peer_id
 load_dotenv("config.env")
+
+
+def paginate_help(page_number, loaded_modules, prefix):
+    number_of_rows = 5
+    number_of_cols = 2
+    helpable_modules = []
+    for p in loaded_modules:
+        if not p.startswith("_"):
+            helpable_modules.append(p)
+    helpable_modules = sorted(helpable_modules)
+    modules = [custom.Button.inline(
+        "{} {}".format("🔸", x),
+        data="ub_modul_{}".format(x))
+        for x in helpable_modules]
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
+    if len(modules) % number_of_cols == 1:
+        pairs.append((modules[-1],))
+    max_num_pages = ceil(len(pairs) / number_of_rows)
+    modulo_page = page_number % max_num_pages
+    if len(pairs) > number_of_rows:
+        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
+            [
+            (custom.Button.inline("⬅️", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("➡️", data="{}_next({})".format(prefix, modulo_page)))
+        ]
+    return pairs
+
 
 # Bot Logs setup:
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+ASYNC_POOL = []
 
 if CONSOLE_LOGGER_VERBOSE:
     basicConfig(
@@ -53,11 +85,10 @@ if CONFIG_CHECK:
 API_KEY = os.environ.get("API_KEY", None)
 API_HASH = os.environ.get("API_HASH", None)
 
-
-# Userbot Session String
+# UserBot Session String
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 
-# Logging channel/group ID configuration.
+# Logging channel/group ID configuration
 BOTLOG_CHATID = int(os.environ.get("BOTLOG_CHATID", None))
 
 # Userbot logging feature switch.
@@ -74,7 +105,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/adekmaulana/ProjectBish.git")
+    "https://github.com/AnggaR96s/DCCLXVI.git")
 # UPSTREAM_REPO_URL branch, the default is master
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "master")
@@ -88,16 +119,16 @@ DB_URI = os.environ.get("DATABASE_URL", None)
 # OCR API key
 OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
 
+# remove.bg API key
+REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+
 # Telegraph
 TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", None)
 
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
-# remove.bg API key
-REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-
-# Chrome Driver and Headless Google Chrome Binaries
+# Chrome sürücüsü ve Google Chrome dosyaları
 CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
 GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
 
@@ -105,15 +136,15 @@ GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
 OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
 WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY", None)
 
-# Anti Spambot Config
+# Lydia API
+LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
+
+# Anti Spambot
 ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
 ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
 
 # Youtube API key
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-
-# Default .alive name
-ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", ""))
@@ -157,6 +188,15 @@ GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN", None)
 # Quote
 QUOTES_API_TOKEN = os.environ.get("QUOTES_API_TOKEN", None)
 
+# Inline bot
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
+
+# Genius Lyrics API
+GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN", None)
+CMD_HELP = {}
+
+
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
 # and giving them correct perms to work properly.
 if not os.path.exists('bin'):
@@ -183,17 +223,40 @@ else:
     bot = TelegramClient("userbot", API_KEY, API_HASH)
 
 
+if os.path.exists("learning-data-root.check"):
+    os.remove("learning-data-root.check")
+else:
+    LOGS.info("No Braincheck file, fetching ...")
+
+URL = 'https://akmj.website/learning-data-root.check'
+
+with open('learning-data-root.check', 'wb') as load:
+    load.write(get(URL).content)
+    
+
+if os.path.exists("blacklist.check"):
+    os.remove("blacklist.check")
+else:
+    LOGS.info("No Blacklist check file, fetching ...")
+
+URL = 'https://akmj.website/blacklist.check'
+
+with open('blacklist.check', 'wb') as load:
+    load.write(get(URL).content)    
+
+
 async def check_botlog_chatid():
     if not BOTLOG_CHATID and LOGSPAMMER:
         LOGS.info(
             "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the private error log storage to work."
-        )
+            )
         quit(1)
 
     elif not BOTLOG_CHATID and BOTLOG:
         LOGS.info(
-            "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the userbot logging feature to work."
-        )
+            "You must set up the BOTLOG_CHATID variable in the config.env or environment variables, for the private error log storage to work."
+
+            )
         quit(1)
 
     elif not BOTLOG or not LOGSPAMMER:
@@ -209,18 +272,214 @@ async def check_botlog_chatid():
 
 with bot:
     try:
+        bot(JoinChannelRequest("@akmjfeels"))
+        bot(JoinChannelRequest("@userbugidspm"))       
+        
+        tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=API_KEY,
+            api_hash=API_HASH
+        ).start(bot_token=BOT_TOKEN)
+
+        dugmeler = CMD_HELP
+        me = bot.get_me()
+        uid = me.id
+
+        @tgbot.on(events.NewMessage(pattern='/start'))
+        async def handler(event):
+            if not event.message.from_id == uid:
+                await event.reply(f'`DCLXVI UserBot by` @NGGDCLXVI`! (`@{me.username}`) I am here to help you.`')
+            else:
+                await event.reply(f'`I work for you :) I love you. ❤️`')
+
+        @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
+        async def inline_handler(event):
+            builder = event.builder
+            result = None
+            query = event.text
+            if event.query.user_id == uid and query.startswith("@akmjfeels"):
+                rev_text = query[::-1]
+                buttons = paginate_help(0, dugmeler, "helpme")
+                result = builder.article(
+                    f"Please Use Only With .help Command",
+                    text="{}\nTotal loaded modules: {}".format(
+                        "DCLXVI UserBot by @akmjfeels\n\nhttps://github.com/AnggaR96s/DCLXVI", len(dugmeler)),
+                    buttons=buttons,
+                    link_preview=False
+                )
+            elif query.startswith("tb_btn"):
+                result = builder.article(
+                    "© @akmjfeels",
+                    text=f"@akmjfeels",
+                    buttons=[],
+                    link_preview=True
+                )
+            else:
+                result = builder.article(
+                    "© @akmjfeels",
+                    text="""@akmjfeels is for you!
+You can convert your account to bot and use them. Remember, you can't manage someone else's bot! All installation details are explained from GitHub address below.""",
+                    buttons=[
+                        [custom.Button.url("Follow Channel", "https://t.me/akmjfeels"), custom.Button.url(
+                            "Build by", "https://t.me/NGGDCLXVI")],
+                        [custom.Button.url(
+                            "GitHub", "https://github.com/AnggaR96s/DCLXVI")]
+                    ],
+                    link_preview=False
+                )
+            await event.answer([result] if result else None)
+
+        @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(b"helpme_next\((.+?)\)")
+        ))
+        async def on_plug_in_callback_query_handler(event):
+            if event.query.user_id == uid:  # pylint:disable=E0602
+                current_page_number = int(
+                    event.data_match.group(1).decode("UTF-8"))
+                buttons = paginate_help(
+                    current_page_number + 1, dugmeler, "helpme")
+                # https://t.me/TelethonChat/115200
+                await event.edit(buttons=buttons)
+            else:
+                reply_pop_up_alert = "Please make for yourself, don't use my bot!"
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+        @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(b"helpme_prev\((.+?)\)")
+        ))
+        async def on_plug_in_callback_query_handler(event):
+            if event.query.user_id == uid:  # pylint:disable=E0602
+                current_page_number = int(
+                    event.data_match.group(1).decode("UTF-8"))
+                buttons = paginate_help(
+                    current_page_number - 1,
+                    dugmeler,  # pylint:disable=E0602
+                    "helpme"
+                )
+                # https://t.me/TelethonChat/115200
+                await event.edit(buttons=buttons)
+            else:
+                reply_pop_up_alert = "Please make for yourself, don't use my bot!"
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+        @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(b"ub_modul_(.*)")
+        ))
+        async def on_plug_in_callback_query_handler(event):
+            if event.query.user_id == uid:  # pylint:disable=E0602
+                modul_name = event.data_match.group(1).decode("UTF-8")
+
+                cmdhel = str(CMD_HELP[modul_name])
+                if len(cmdhel) > 90:
+                    help_string = str(CMD_HELP[modul_name])[
+                        :90] + "\n\nRead more... " + modul_name + " "
+                else:
+                    help_string = str(CMD_HELP[modul_name])
+
+                reply_pop_up_alert = help_string if help_string is not None else \
+                    "{} No document has been written for module.".format(
+                        modul_name)
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            else:
+                reply_pop_up_alert = "Please make for yourself, don't use my bot!"
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+    except:
+        LOGS.info(
+            "Support for inline is disabled on your bot. "
+            "To enable it, define a bot token and enable inline mode on your bot. "
+            "If you think there is a problem other than this, contact us."
+        )
+
+    try:
         bot.loop.run_until_complete(check_botlog_chatid())
     except:
         LOGS.info(
-            "BOTLOG_CHATID environment variable isn't a "
-            "valid entity. Check your environment variables/config.env file.")
+            "ERROR: The BOTLOG_CHATID variable entered is not valid. "
+            "Please check the value you entered. "
+            "Bot is crashed .."
+        )
         quit(1)
+
 
 # Global Variables
 COUNT_MSG = 0
 USERS = {}
+BRAIN_CHECKER = []
+BLACKLIST = []
 COUNT_PM = {}
 LASTMSG = {}
-CMD_HELP = {}
+ENABLE_KILLME = True
 ISAFK = False
 AFKREASON = None
+ZALG_LIST = [[
+    "̖",
+    " ̗",
+    " ̘",
+    " ̙",
+    " ̜",
+    " ̝",
+    " ̞",
+    " ̟",
+    " ̠",
+    " ̤",
+    " ̥",
+    " ̦",
+    " ̩",
+    " ̪",
+    " ̫",
+    " ̬",
+    " ̭",
+    " ̮",
+    " ̯",
+    " ̰",
+    " ̱",
+    " ̲",
+    " ̳",
+    " ̹",
+    " ̺",
+    " ̻",
+    " ̼",
+    " ͅ",
+    " ͇",
+    " ͈",
+    " ͉",
+    " ͍",
+    " ͎",
+    " ͓",
+    " ͔",
+    " ͕",
+    " ͖",
+    " ͙",
+    " ͚",
+    " ",
+],
+    [
+    " ̍", " ̎", " ̄", " ̅", " ̿", " ̑", " ̆", " ̐", " ͒", " ͗",
+    " ͑", " ̇", " ̈", " ̊", " ͂", " ̓", " ̈́", " ͊", " ͋", " ͌",
+    " ̃", " ̂", " ̌", " ͐", " ́", " ̋", " ̏", " ̽", " ̉", " ͣ",
+    " ͤ", " ͥ", " ͦ", " ͧ", " ͨ", " ͩ", " ͪ", " ͫ", " ͬ", " ͭ",
+    " ͮ", " ͯ", " ̾", " ͛", " ͆", " ̚"
+],
+    [
+    " ̕",
+    " ̛",
+    " ̀",
+    " ́",
+    " ͘",
+    " ̡",
+    " ̢",
+    " ̧",
+    " ̨",
+    " ̴",
+    " ̵",
+    " ̶",
+    " ͜",
+    " ͝",
+    " ͞",
+    " ͟",
+    " ͠",
+    " ͢",
+    " ̸",
+    " ̷",
+    " ͡",
+]]
--- userbot/events.py
+++ userbot/events.py
@@ -1,33 +1,46 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2020 TeamDerUntergang.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-""" Userbot module for managing events.
- One of the main components of the userbot. """
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+""" Olayları yönetmek için UserBot modülü.
+ UserBot'un ana bileşenlerinden biri. """
 
 import sys
+
 from asyncio import create_subprocess_shell as asyncsubshell
 from asyncio import subprocess as asyncsub
 from os import remove
 from time import gmtime, strftime
 from traceback import format_exc
-
 from telethon import events
 
-from userbot import bot, BOTLOG_CHATID, LOGSPAMMER
-
+from userbot import bot, BOTLOG_CHATID, LOGSPAMMER, BLACKLIST
 
 def register(**args):
-    """ Register a new event. """
+    """ Yeni bir etkinlik kaydedin. """
     pattern = args.get('pattern', None)
     disable_edited = args.get('disable_edited', False)
     ignore_unsafe = args.get('ignore_unsafe', False)
     unsafe_pattern = r'^[^/!#@\$A-Za-z]'
     groups_only = args.get('groups_only', False)
     trigger_on_fwd = args.get('trigger_on_fwd', False)
+    trigger_on_inline = args.get('trigger_on_inline', False)
     disable_errors = args.get('disable_errors', False)
-    insecure = args.get('insecure', False)
+    me = bot.get_me()
+    uid = me.id
+    uid not in BLACKLIST
 
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
@@ -46,9 +59,9 @@ def register(**args):
 
     if "trigger_on_fwd" in args:
         del args['trigger_on_fwd']
-
-    if "insecure" in args:
-        del args['insecure']
+      
+    if "trigger_on_inline" in args:
+        del args['trigger_on_inline']
 
     if pattern:
         if not ignore_unsafe:
@@ -56,10 +69,6 @@ def register(**args):
 
     def decorator(func):
         async def wrapper(check):
-            if check.edit_date and check.is_channel and not check.is_group:
-                # Messages sent in channels can be edited by other users.
-                # Ignore edits that take place in channels.
-                return
             if not LOGSPAMMER:
                 send_to = check.chat_id
             else:
@@ -68,36 +77,30 @@ def register(**args):
             if not trigger_on_fwd and check.fwd_from:
                 return
 
+            if check.via_bot_id and not trigger_on_inline:
+                return
+             
             if groups_only and not check.is_group:
                 await check.respond("`I don't think this is a group.`")
                 return
 
-            if check.via_bot_id and not insecure and check.out:
-                return
-
             try:
-                await func(check)
-
-            # Thanks to @kandnub for this HACK.
-            # Raise StopPropagation to Raise StopPropagation
-            # This needed for AFK to working properly
-
+                if uid not in BLACKLIST:
+                    await func(check)
+                else:
+                    raise RetardsException()
             except events.StopPropagation:
                 raise events.StopPropagation
-            # This is a gay exception and must be passed out. So that it doesnt spam chats
+            except RetardsException:
+                exit(1)
             except KeyboardInterrupt:
                 pass
-            except BaseException:
-
-                # Check if we have to disable it.
-                # If not silence the log spam on the console,
-                # with a dumb except.
-
+            except:
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT ERROR REPORT**\n"
-                    link = "Support chat PM: @adekmaulana"
+                    link = "Support chat PM: @NGGDCLXVI"
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
                     text += "Nothing is logged except the fact of error and date\n"
@@ -134,22 +137,18 @@ def register(**args):
 
                     ftext += result
 
-                    file = open("error.log", "w+")
+                    file = open("crash.log", "w+")
                     file.write(ftext)
                     file.close()
 
                     if LOGSPAMMER:
-                        await check.client.respond(
-                            "`Sorry, my userbot has crashed."
-                            "\nThe error logs are stored in the userbot's log chat.`"
-                        )
+                        await check.client.respond("`Sorry, userbot has crashed..\
+                        \nThe error logs are stored in the userbot's log chat.`")
 
-                        await check.client.send_file(send_to,
-                                                     "error.log",
-                                                     caption=text)
-                        remove("error.log")
-            else:
-                pass
+                    await check.client.send_file(send_to,
+                                                 "crash.log",
+                                                 caption=text)
+                    remove("crash.log")
 
         if not disable_edited:
             bot.add_event_handler(wrapper, events.MessageEdited(**args))
@@ -157,3 +156,6 @@ def register(**args):
         return wrapper
 
     return decorator
+
+class RetardsException(Exception):
+    pass
