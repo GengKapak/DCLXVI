f158b7136ebba31edd785cdf64d7227aada6fc61
Angga <angga@linuxmail.org>
2020-05-18 17:50:19 +0700

    misc: .repo: change url
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -99,7 +99,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Repo](https://github.com/adekmaulana/ProjectBish) GitHub's page."
+        "[Repo](https://github.com/AnggaR96s/DCLXVI) GitHub's page."
     )
 
 
