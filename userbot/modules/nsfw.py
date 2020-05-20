ce5a44209950097e0203dec58cef33486d92d226
Angga <angga@linuxmail.org>
2020-05-20 17:27:09 +0700

    nsfw: add some shit event
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/nsfw.py
+++ userbot/modules/nsfw.py
@@ -10,6 +10,7 @@ from urllib.request import urlopen
 import requests
 from bs4 import BeautifulSoup
 import re
+from asyncio import sleep
 from telethon.tl.types import MessageMediaPhoto
 from PIL import Image
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
@@ -19,6 +20,9 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^.boobs(?: |$)(.*)")
 async def boobs(e):
+    await e.edit("`Finding some big boobs...`")
+    await sleep(3)
+    await e.edit("`Sending some big boobs...`")
     nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
     urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), "*.jpg")
     os.rename('*.jpg', 'boobs.jpg')
@@ -28,6 +32,9 @@ async def boobs(e):
 
 @register(outgoing=True, pattern="^.butts(?: |$)(.*)")
 async def butts(e):
+    await e.edit("`Finding some beautiful butts...`")
+    await sleep(3)
+    await e.edit("`Sending some beautiful butts...`")
     nsfw = requests.get('http://api.obutts.ru/noise/1').json()[0]["preview"]
     urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), "*.jpg")
     os.rename('*.jpg', 'butts.jpg')
