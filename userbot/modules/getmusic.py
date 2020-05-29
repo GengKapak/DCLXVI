4eb6cafc683b45295248220ff661e03a51c72253
aidilaryanto <aidilaryanto18@gmail.com>
2020-05-29 12:51:24 +0800

    getmusic: add MusicHunter module
    Change-Id: Icbc0265fbb26be7d4143adbd28af918d851e9110
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -98,6 +98,33 @@ async def _(event):
                                        [msg.id, response.id, respond.id])
     await event.delete()
 
+@register(outgoing=True, pattern="^.sdd(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    d_link = event.pattern_match.group(1)
+    if ".com" not in d_link:
+        await event.edit("` I need a link to download something pro.`**(._.)**")
+    else:
+        await event.edit("**Initiating Download!**")
+    chat = "@MusicHuntersBot"
+    async with bot.conversation(chat) as conv:
+          try:
+              msg_start = await conv.send_message("/start")
+              response = await conv.get_response()
+              msg = await conv.send_message(d_link)
+              details = await conv.get_response()
+              song = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await event.edit("**Error:** `unblock` @MusicHuntersBot `and retry!`")
+              return
+          await bot.send_file(event.chat_id, song, caption=details.text)
+          await event.client.delete_messages(conv.chat_id,
+                                             [msg_start.id, response.id, msg.id, details.id, song.id])
+          await event.delete()
+
 CMD_HELP.update({
     "song":
         ">`.song` **Artist - Song Title**"
@@ -105,5 +132,7 @@ CMD_HELP.update({
         ">`.smd` **Artist - Song Title**"
         "\nUsage: Download music from spotify.\n"
         ">`.net` **Artist - Song Title**"
-        "\nUsage: Download music with @WooMaiBot."
+        "\nUsage: Download music with @WooMaiBot.\n"
+        ">`.sdd <Spotify/Deezer Link>`"
+        "\nUsage: Download music from Spotify or Deezer."
 })
