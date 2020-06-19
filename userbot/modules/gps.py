0c76af7865f799db23d778f4687ffdc6f68616c0
Angga <angga@linuxmail.org>
2020-06-20 01:48:36 +0700

    DCLXVI: add gps module
    Change-Id: I2c55568c5a4086730684e85cb6271eb02e91ae00
    Signed-off-by: Angga <angga@linuxmail.org>

---
+++ userbot/modules/gps.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2020 GengKapak and AnggaR96s.
+# All rights reserved.
+
+from geopy.geocoders import Nominatim
+from telethon.tl import types
+from userbot.events import register
+from userbot import CMD_HELP
+
+
+@register(outgoing=True, pattern="^\.gps (.*)")
+async def gps(e):
+    if e.fwd_from:
+        return
+    reply_to_id = e.message.id
+    if e.reply_to_msg_id:
+        reply_to_id = e.reply_to_msg_id
+    input_str = e.pattern_match.group(1)
+
+    if not input_str:
+        return await e.edit("`What should i find give me location.`")
+
+    await e.edit("`Finding`")
+
+    geolocator = Nominatim(user_agent="GengKapak")
+    geoloc = geolocator.geocode(input_str)
+    if geoloc:
+        lon = geoloc.longitude
+        lat = geoloc.latitude
+        await e.reply(
+            input_str,
+            file=types.InputMediaGeoPoint(
+                types.InputGeoPoint(
+                    lat, lon
+                )
+            ),
+            reply_to=reply_to_id
+        )
+        await e.delete()
+    else:
+        await e.edit("`I coudn't find it.`")
+
+
+CMD_HELP.update({
+    "gps":
+    ">`.gps` **Location**"
+    "\nUsage: Search location."
+})
