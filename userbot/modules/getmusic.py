b51b2acc930b98442ffbee0e2cdc4a2999ffa706
Angga <angga@linuxmail.org>
2020-07-04 14:24:30 +0700

    getmusic: remove song module
    Change-Id: Id60e73acf09f3e1eda5c2a7d8ec518e9d02568d0
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -1,44 +1,8 @@
-# AnggaR96s
-
 from telethon import events
-import subprocess
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 import asyncio
 from userbot.events import register
 from userbot import bot, CMD_HELP
-import glob
-import os
-
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    os.system("instantmusic -q -s "+name)
-
-@register(outgoing=True, pattern=r"^\.song (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    cmd = event.pattern_match.group(1)
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    await event.edit("`Ok finding the song..`")
-    bruh(str(cmd))
-    l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.edit("`Sending song..`")
-    await event.client.send_file(
-                event.chat_id,
-                loa,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id
-            )
-    os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
-    await event.delete()
 
 
 @register(outgoing=True, pattern="^\.smd(?: |$)(.*)")
@@ -119,8 +83,6 @@ async def _(event):
 
 CMD_HELP.update({
     "song":
-        ">`.song` **Artist - Song Title**"
-        "\nUsage: Finding and uploading song.\n"
         ">`.smd` **Artist - Song Title**"
         "\nUsage: Download music from spotify.\n"
         ">`.net` **Artist - Song Title**"
