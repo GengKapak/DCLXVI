0478cdad3b8276e0372ab7b2300c6fe3ad38a494
Angga <angga@linuxmail.org>
2020-07-09 14:16:02 +0700

    getmusic: add song module
    Change-Id: Ic2a27d527fe44ab7681d5f630e635b2a02d821b3
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -1,8 +1,88 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import glob
+import subprocess
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 import asyncio
 from userbot.events import register
 from userbot import bot, CMD_HELP
+
+# For song module
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+def getmusic(get, DEFAULT_AUDIO_QUALITY):
+  search = get
+
+  headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+
+  html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
+  soup = BeautifulSoup(html, 'html.parser')
+  for link in soup.find_all('a'):
+    if '/watch?v=' in link.get('href'):
+        # May change when Youtube Website may get updated in the future.
+        video_link = link.get('href')
+        break
+
+  video_link =  'http://www.youtube.com/'+video_link
+  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' +DEFAULT_AUDIO_QUALITY + ' ' + video_link)
+  os.system(command)
+
+
+@register(outgoing=True, pattern=r"^\.song (.*)")
+async def _(event):
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+        await event.edit("`Wait..! I am finding your song..`")
+    elif reply.message:
+        query = reply.message
+        await event.edit("`Wait..! I am finding your song..`")
+    else:
+    	await event.edit("`What I am Supposed to find`")
+    	return
+
+    getmusic(str(query),"320k")
+    l = glob.glob("*.mp3")
+    loa = l[0]
+    await event.edit("`Yeah.. Uploading your song..`")
+    await event.client.send_file(
+                event.chat_id,
+                loa,
+                force_document=True,
+                allow_cache=False,
+                caption=query,
+                reply_to=reply_to_id
+            )
+    await event.delete()
+    os.system("rm -rf *.mp3")
+    subprocess.check_output("rm -rf *.mp3",shell=True)
 
 
 @register(outgoing=True, pattern="^\.smd(?: |$)(.*)")
@@ -83,6 +163,8 @@ async def _(event):
 
 CMD_HELP.update({
     "song":
+        ">`.song` **Artist - Song Title**"
+        "\nUsage: Finding and uploading song.\n"
         ">`.smd` **Artist - Song Title**"
         "\nUsage: Download music from spotify.\n"
         ">`.net` **Artist - Song Title**"
