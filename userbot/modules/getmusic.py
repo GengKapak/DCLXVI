a553549e7437f6a3ebe207716e7ab31787308d41
Angga <angga@linuxmail.org>
2020-07-12 20:38:48 +0700

    make codacy smile
    Change-Id: I53b8a776a7ad68d6a542de43a0dfe1c18905520b
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -8,11 +8,6 @@ from bs4 import BeautifulSoup
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon import events
-from telethon.errors import (
-    MessageEmptyError,
-    MessageNotModifiedError,
-    MessageTooLongError,
-)
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.types import DocumentAttributeVideo
 
