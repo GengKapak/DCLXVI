4ef9c62262c682b926c5b57e85e989f8a49f4967
Angga <angga@linuxmail.org>
2020-07-27 17:52:20 +0700

    sangmata: improvement
    * avoid crash if sangmata bot is dead
    
    Change-Id: I84771d33589d80a25a81280c4e9fd77fb017c799
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -1,3 +1,5 @@
+from asyncio.exceptions import TimeoutError
+
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
@@ -18,24 +20,27 @@ async def _(event):
         return
     chat = "@SangMataInfo_bot"
     await event.edit("`Processing`")
-    async with bot.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=461843263)
-            )
-            await bot.forward_messages(chat, reply_message)
-            response = await response
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.reply("`Please unblock @sangmatainfo_bot and try again`")
-            return
-        if response.text.startswith("Forward"):
-            await event.edit(
-                "`Can you kindly disable your forward privacy settings for good?`"
-            )
-        else:
-            await event.edit(f"{response.message.message}")
-            await bot.send_read_acknowledge(conv.chat_id)
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=461843263)
+                )
+                await bot.forward_messages(chat, reply_message)
+                response = await response
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Please unblock @sangmatainfo_bot and try again`")
+                return
+            if response.text.startswith("Forward"):
+                await event.edit(
+                    "`Can you kindly disable your forward privacy settings for good?`"
+                )
+            else:
+                await event.edit(f"{response.message.message}")
+                await bot.send_read_acknowledge(conv.chat_id)
+    except TimeoutError:
+        return await event.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
 
 
 CMD_HELP.update(
