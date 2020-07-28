04b34a7228406ba7c59139d8dd4807e975db2250
KenHV <kenhv@null.net>
2020-07-29 03:48:55 +0700

    scrapers: yt: improvements
    - introduce counter feature
    - better results
    - cleanup code
    
    Change-Id: I91844e18c0290150dd84e5a17aafdb30b5f13b02
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -484,23 +484,49 @@ async def lang(value):
         )
 
 
-@register(outgoing=True, pattern=r"^\.yt (.*)")
-async def yt_search(video_q):
+@register(outgoing=True, pattern=r"^\.yt (\d*) *(.*)")
+async def yt_search(event):
     """ For .yt command, do a YouTube search from Telegram. """
-    query = video_q.pattern_match.group(1)
+
+    if event.pattern_match.group(1) != "":
+        counter = int(event.pattern_match.group(1))
+        if counter > 10:
+            counter = int(10)
+        if counter <= 0:
+            counter = int(1)
+    else:
+        counter = int(3)
+
+    query = event.pattern_match.group(2)
+
     if not query:
-        await video_q.edit("`Enter query to search`")
-    await video_q.edit("`Processing...`")
+        return await event.edit("`Enter a query to search.`")
+    await event.edit("`Processing...`")
+
     try:
-        results = json.loads(YoutubeSearch(query, max_results=7).to_json())
+        results = json.loads(
+            YoutubeSearch(
+                query,
+                max_results=counter).to_json())
     except KeyError:
-        return await video_q.edit(
+        return await event.edit(
             "`Youtube Search gone retard.\nCan't search this query!`"
         )
-    output = f"**Search Query:**\n`{query}`\n\n**Results:**\n\n"
+
+    output = f"**Search Query:**\n`{query}`\n\n**Results:**\n"
+
     for i in results["videos"]:
-        output += f"● `{i['title']}`\nhttps://www.youtube.com{i['url_suffix']}\n\n"
-    await video_q.edit(output, link_preview=False)
+        try:
+            title = i["title"]
+            link = "https://youtube.com" + i["url_suffix"]
+            channel = i["channel"]
+            duration = i["duration"]
+            views = i["views"]
+            output += f"[{title}]({link})\nChannel: `{channel}`\nDuration: {duration} | {views}\n\n"
+        except IndexError:
+            break
+
+    await event.edit(output, link_preview=False)
 
 
 @register(outgoing=True, pattern=r"^\.r(a|v) (.*)")
@@ -659,8 +685,9 @@ CMD_HELP.update(
         "trt": ">`.trt <text> [or reply]`"
         "\nUsage: Translates text to the language which is set."
         "\nUse >`.lang trt <language code>` to set language for trt. (Default is English)",
-        "yt": ">`.yt <text>`"
-        "\nUsage: Does a YouTube search.",
+        "yt": ">`.yt <count> <query>`"
+        "\nUsage: Does a YouTube search."
+        "\nCan specify the number of results needed (default is 3).",
         "imdb": ">`.imdb <movie-name>`"
         "\nUsage: Shows movie info and other stuff.",
         "rip": ">`.ra <url> or .rv <url>`"
