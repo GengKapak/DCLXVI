60e7255da1eda58df9dde1b39a1e18e2e10f48a2
Angga <angga@linuxmail.org>
2020-07-28 21:57:00 +0700

    inline help: fix if not have permission to send inline bot request
    Change-Id: I441ef11289b4ff4c9164fe5093de913663bfe0bb
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -19,6 +19,7 @@
 
 import logging
 
+
 from userbot import BOT_USERNAME
 from userbot.events import register
 
@@ -29,14 +30,19 @@ logging.basicConfig(
 
 @register(outgoing=True, pattern=r"^\.helpme")
 async def yardim(event):
-    tgbotusername = BOT_USERNAME
-    if tgbotusername is not None:
-        results = await event.client.inline_query(tgbotusername, "@GengKapak")
-        await results[0].click(
-            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
-        )
-        await event.delete()
-    else:
-        await event.edit(
-            "`The bot doesn't work! Please set the Bot Token and Username correctly. The module has been stopped.`"
+    try:
+        tgbotusername = BOT_USERNAME
+        if tgbotusername is not None:
+            results = await event.client.inline_query(tgbotusername, "@GengKapak")
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        else:
+            await event.edit(
+                "`The bot doesn't work! Please set the Bot Token and Username correctly. The module has been stopped.`"
+            )
+    except Exception:
+        return await event.edit(
+            "`You cannot send inline results in this chat (caused by SendInlineBotResultRequest)`"
         )
